7bf02e5d47b89b259e75c14ac7bbbdcc937074db
Vladimir Privalov <vovaprivalov@gmail.com>
2018-12-01 19:55:42 +0100

    Add input arguments validation and documentation, modified error handling in Web API application. Modified README.

--- README.md
+++ README.md
@@ -29,6 +29,7 @@ CLI client is represented by converter-cli.py. To run CLI application:
 ```bash
 ./currency_converter.py --amount 100.0 --input_currency EUR --output_currency CZK
 ```
+--amount and --input_currency arguments are required, --output_currency argument is optional. If --output_currency is missing, amount will be converted to all known currencies.
 
 ## Web client
 Web API client is represented by converter-web.py. 
@@ -36,3 +37,8 @@ Flask using Python 2.7 on default. To run web application using Python 3:
 ```bash
 ./converter-web.py
 ```
+Open following URL in browser:
+```bash
+http://127.0.0.1:5000/currency_converter?amount=20&&input_currency=RUB&&output_currency=CZK
+```
+amount and input_currency arguments are required, output_currency argument is optional. If output_currency is missing, amount will be converted to all known currencies.
--- app/converter-web.py
+++ app/converter-web.py
@@ -4,40 +4,47 @@ from flask import Flask, request, Response
 import re
 from converter import Converter
 
+# Create the application instance
 app = Flask(__name__)
 
-class InputParameterError(Exception):
+# Create custom exception class
+class CustomError(Exception):
 	"""Input parameter error."""
 
-class CurrencyException(Exception):
-	"""Currency interpetation error."""
-
-@app.errorhandler(CurrencyException)
-def handle_currency_exception(error):
+# Create custom error handler for errors in application
+@app.errorhandler(CustomError)
+def handle_custom_exception(error):
 	details = error.args[0]
 	resp = Response(details['message'], status=200, mimetype='text/plain')
 	return resp
 
-@app.errorhandler(InputParameterError)
-def handle_input_parameter_error(error):
-	details = error.args[0]
-	resp = Response('Input parameter is invalid: ' + details['message'], status=200, mimetype='text/plain')
-	return resp
-
+# Create a URL route in our application for "/currency_converter"
 @app.route('/currency_converter')
 def currency_converter():
+	"""
+	This function just responds to the browser ULR
+	localhost:5000/currency_converter
+
+	:return:        the CurrencyConverter result as a JSON or an error as plain text
+	"""
 	amount = request.args.get('amount', type=float)
 	in_currency = request.args.get('input_currency', type=str)
 	out_currency = request.args.get('output_currency', type=str)
 
-	# Validate amount parameter
+	# Validate input parameters
+	if amount is None:
+		raise CustomError({ 'message': 'Error: The following arguments are required: amount' })
+
 	if re.match("^\d+?\.\d+?$", str(amount)) is None:
-		raise InputParameterError({ 'message': 'Amount should be float' })
+		raise CustomError({ 'message': 'Input parameter is invalid: Amount should be float' })
+
+	if in_currency is None:
+		raise CustomError({ 'message': 'Error: The following arguments are required: input_currency' })
 
 	currency_converter = Converter()
 	(result, error) = currency_converter.convert(amount, in_currency, out_currency)
 	if result is None:
-		raise CurrencyException({ 'message': error })
+		raise CustomError({ 'message': error })
 	else:
 		resp = Response(result, status=200, mimetype='application/json')
 		return resp
